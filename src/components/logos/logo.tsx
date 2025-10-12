035bb8b8dfc26de34466a9140f2fb6475bfa6b33
Gaya KACI <gaya.kaci2002@hotmail.fr>
2025-10-13 00:37:26 +0200

    beta 1

---
+++ src/components/logos/logo.tsx
@@ -0,0 +1,9 @@
+import { Smartphone } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <Smartphone className="h-8 w-8" />
+    </div>
+  );
+}
